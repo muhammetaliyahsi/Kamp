3c411efa30b50db9d2ad7cf32b33e2f392aca423
Muhammet Ali Yahşi <alleyy@outlook.com.tr>
2021-01-19 20:17:32 +0300

    19.01.2021

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public int Id;
+        public string ad;
+        public string soyad;
+    }
+}
---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ad + musteri.soyad + "Müşterisi eklendi");
+        }
+
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("Müşteriler listelendi");
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ad + musteri.soyad + "Müşterisi silindi");
+        }
+    }
+}
